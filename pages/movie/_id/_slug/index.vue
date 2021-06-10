70d44fec6b338413b081ceae010dbfb8869dc2f2
James Muriithi <muriithijames556@gmail.com>
2021-06-10 14:12:49 +0300

    movie details

--- pages/movie/_id/_slug/index.vue
+++ pages/movie/_id/_slug/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <movie-details />
+    <similar-movies />
   </div>
 </template>
 
 <script>
 import MovieDetails from '@/components/Movie/MovieDetails.vue'
+import SimilarMovies from '@/components/Movie/SimilarMovies.vue'
 
 export default {
   components: {
     MovieDetails,
+    SimilarMovies,
   },
 }
 </script>
